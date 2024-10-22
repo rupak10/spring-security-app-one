b37ca0aab7b77aa7ae66e80b4f4ec20dffdf3c5b
Rupak Kumar Das (Assistant Manager) <rupak@pksf.local>
2024-10-22 16:25:34 +0600

    spring boot security project initialized

---
+++ src/main/java/com/rupak/SpringSecurityAppOneApplication.java
@@ -0,0 +1,13 @@
+package com.rupak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAppOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAppOneApplication.class, args);
+	}
+
+}
